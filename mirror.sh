359f142f91b13c4baefe6c9f8e31e67ac207e6dc
Tim Bateman <tim.bateman@gmail.com>
2012-05-24 21:37:41 -0400

    updated to prevent accidental website mirroring ;)

--- mirror.sh
+++ mirror.sh
@@ -2,10 +2,12 @@
 
 # assume we've got wget?
 
-cd Library/Formula
-mkdir mirror
-cd mirror
-for url in `grep url\ \' ../* | sed "s/^.*url\ \'//g" | sed "s/\'.*$//g" | sort | uniq`
+cd Library/
+mkdir Mirror
+cd Mirror
+grep url\ \' ../Formula/*.rb | sed "s/^.*url\ \'//g" | sed "s/\'.*$//g" | sort | uniq > url_list
+# start with the HTTP
+for url in `cat url_list | grep ^http`
 do
-        wget -m $url
+        wget --timestamping --force-directories $url
 done
