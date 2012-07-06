311a2264a34d99171f643613ead003b6012983b3
Tim Bateman <tim.bateman@gmail.com>
2012-07-06 11:16:35 -0400

    added initial formula fix-up

--- mirror.sh
+++ mirror.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+function fixUpFormula {
+    url=$1
+    newurl=$2
+    # find all the formulas that have this url
+    for f in `grep ${url} ../Formula/*.rb | cut -d':' -f1`
+    do
+        echo "Fixing up ${f}"
+        sed "s*${url}*${newurl}*g" ${f} > ${f}
+    done
+}
+
 # assume we've got wget?
 # if not brew install wget
 
@@ -19,12 +30,16 @@ for url in `cat url_list | grep ^http`
 do
     echo "Downloading ${url}"
     wget --timestamping --force-directories $url > /dev/null
+    newurl=`echo -n "${PWD}/" && ( echo ${url} | sed 's/^.*\:\/\///g' )`
+    fixUpFormula $url $newurl
 done
 # default to HTTP if no protocol
 for url in `cat url_list | grep -v ://`
 do
     echo "Downloading ${url}"
     wget --timestamping --force-directories $url > /dev/null
+    newurl=`echo -n "${PWD}/" && ( echo ${url} | sed 's/^.*\:\/\///g' )`
+    fixUpFormula $url $newurl
 done
 
 # FTP
@@ -32,6 +47,8 @@ for url in `cat url_list | grep ^ftp`
 do
     echo "Downloading ${url}"
     wget --timestamping --force-directories $url > /dev/null
+    newurl=`echo -n "${PWD}/" && ( echo ${url} | sed 's/^.*\:\/\///g' )`
+    fixUpFormula $url $newurl
 done
 
 # git
@@ -40,15 +57,19 @@ do
     echo "Cloning ${url}"
     # assume every repo has a different name :\  *eek*
     git clone --bare ${url} > /dev/null
+
+    fixUpFormula $url $newurl
 done
 
 # svn (who's still using that!)
 for url in `cat url_list |grep ^svn.http`
 do
     echo "Checking out $url"
-    dest=`echo http://closure-compiler.googlecode.com/svn/trunk/ | sed 's/^http:\/\///g' | sed 's/\/.*$//g'`
-    # same as git - we're assuming the repo name is uniq
+    $origurl=$url
     url=`echo ${url} | sed 's/^svn.//g'`
+    dest=`echo ${url} | sed 's/^http:\/\///g' | sed 's/\/.*$//g'`
+    dest=`echo "${PWD}/${dest}"`
     svn co ${url} ${dest} > /dev/null
+    fixUpFormula $origurl $dest
 done
 
